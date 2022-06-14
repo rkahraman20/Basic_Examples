bf209fba8bdebe3d1637659be6a928a081f42459
Ridvan Kahraman <ridvankahraman@proton.me>
2022-06-15 00:08:32 +0300

    1st Basic SQL Example was updated.

---
+++ Basic_SQL_Examples/SQL_Example_1_Tree_Structure_Labeling/SQL_Example_1_Tree_Structure_Labeling_v2.sql
@@ -0,0 +1,21 @@
+
+CREATE DATABASE SQLExamples
+
+USE SQLExamples
+
+CREATE TABLE tree(
+	[node] TINYINT,
+	parent TINYINT NULL
+);
+
+INSERT tree VALUES (1, 2), (2, 5), (3, 5), (4, 3), (5, NULL)
+
+SELECT * FROM tree;
+
+SELECT [node],
+	CASE
+		WHEN parent IS NULL THEN 'Root'
+		WHEN [node] NOT IN (SELECT parent FROM tree WHERE parent IS NOT NULL) THEN 'Leaf'
+        WHEN [node] IN (SELECT parent FROM tree WHERE parent IS NOT NULL) THEN 'Inner'
+    END AS label
+FROM tree;
